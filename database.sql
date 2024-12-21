f86435d6c4e0e8f5f89d5402746c3afb899379f6
Lưu Quang Khải <127973462+luuquangkhai9@users.noreply.github.com>
2024-12-21 19:37:47 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,3 @@
-Downloading n98-magerun2.phar, just a moment...
-n98-magerun2.phar: OK
-mysqldump: Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
 -- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: db    Database: magento
